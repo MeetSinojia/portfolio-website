4405dcc0cd0877126ebce291ce074661eea81f54
MeetSinojia <sinojiameet321@gmail.com>
2026-02-24 19:46:08 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    copyPublicDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        swagger: './swagger.html',
+        simple: './simple.html'
+      }
+    }
+  },
+  server: {
+    middlewareMode: false
+  }
 })
